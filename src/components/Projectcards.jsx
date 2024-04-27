74a4455a29633572a122570a5625d99ad79d50f2
MiaMarmeladenbrot <mia.mecklenburg@gmx.net>
2024-04-27 14:50:25 +0200

    slider done

--- src/components/Projectcards.jsx
+++ src/components/Projectcards.jsx
@@ -1,8 +1,18 @@
 import projects from "../assets/data/projects.js";
 
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/effect-cards";
+import "swiper/css/pagination";
+
+// import required modules
+import { Autoplay, EffectCards, Pagination } from "swiper/modules";
+
 const Projectcards = () => {
   return (
-    // # slide funktion
     <section className="flex justify-center flex-col items-center mb-24">
       <h2 className="text-2xl border-brightColor border-b-2 inline-block pb-2 mb-5">
         Projects
@@ -11,24 +21,35 @@ const Projectcards = () => {
         See all
       </a>
 
-      <article className="flex items-center justify-center gap-11">
-        {projects.slice(0, 3).map((singleProject, index) => (
-          <div
+      {/* <article className="flex items-center justify-center gap-11"> */}
+      <Swiper
+        className={" h-[20rem] w-[900px]"}
+        effect={"cards"}
+        grabCursor={true}
+        modules={[EffectCards, Autoplay, Pagination]}
+        loop={true}
+        autoplay={{
+          delay: 3000,
+          disableOnInteraction: true,
+          pauseOnMouseEnter: true,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+      >
+        {projects.slice(0, 5).map((singleProject, index) => (
+          <SwiperSlide
             key={index}
-            className="flex gap-4 items-center justify-center rounded-lg overflow-hidden shadow-[0px_0px_30px_-10px_rgba(0,0,0,0.3)]"
-            // # alle Karten gleich breit
+            className="flex gap-10 items-center rounded-lg overflow-hidden shadow-[0rem_0rem_1.875rem_-0.625rem_rgba(0,0,0,0.3)] bg-whiteColor"
           >
             <img
-              className="w-96 object-contain	"
+              className="w-1/2"
               src={singleProject.image}
               alt={singleProject.title}
             />
             <section className="pr-3 min-w-96">
               <h2 className="mb-5 text-xl">{singleProject.title}</h2>
-              <p className="mb-3">
-                created with
-                {/* {singleProject.skills.map((item) => item)} */}
-              </p>
+              <p className="mb-3"></p>
               <div className="flex flex-wrap gap-2 mb-10">
                 {singleProject.skills.map((item, index) => (
                   <img
@@ -42,8 +63,9 @@ const Projectcards = () => {
 
               <div className="mb-8">
                 <a
-                  className="border border-brightColor text-brightColor hover:bg-darkColor p-2 rounded-lg mr-3"
+                  className="border border-brightColor text-brightColor hover:text-brightColor hover:bg-darkColor p-2 rounded-lg mr-3"
                   href={singleProject.site}
+                  target="_blank"
                 >
                   Live Site
                 </a>
@@ -51,6 +73,7 @@ const Projectcards = () => {
                   // className="bg-brightColor hover:border hover:border-black p-2 rounded-lg"
                   className="border border-brightColor text-brightColor hover:text-brightColor hover:bg-darkColor p-2 rounded-lg mr-3"
                   href={singleProject.repo}
+                  target="_blank"
                 >
                   Repository on GitHub
                 </a>
@@ -62,9 +85,9 @@ const Projectcards = () => {
                 More Info
               </a>
             </section>
-          </div>
+          </SwiperSlide>
         ))}
-      </article>
+      </Swiper>
     </section>
   );
 };
